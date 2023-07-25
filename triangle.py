c0158a7b53276bae56bd1bf20412d28248ab61c3
scranley <80985721+scranley@users.noreply.github.com>
2023-07-25 09:22:40 -0700

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,29 +1,20 @@
 import bpy
 
+# Enable the MB-Lab add-on
+bpy.ops.preferences.addon_enable(module="MB-Lab")
+
 # Clear all mesh objects
 bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_by_type(type='MESH')
 bpy.ops.object.delete()
 
-# Define the 3 vertices of the triangle
-verts = [(0, 0, 0), (1, 0, 0), (0.5, 1, 0)]
+# Create a human shape
+bpy.ops.mbast.init_character()
 
-# Define the single face of the triangle
-faces = [(0, 1, 2)]
-
-# Create the mesh and object
-mesh = bpy.data.meshes.new(name="TriangleMesh")
-obj = bpy.data.objects.new("Triangle", mesh)
-
-# Link the object to the scene
-bpy.context.collection.objects.link(obj)
-
-# Create the triangle
-mesh.from_pydata(verts, [], faces)
-
-# Select the triangle object
-bpy.context.view_layer.objects.active = obj
-obj.select_set(True)
+# Select the human object
+human = bpy.context.object
+bpy.context.view_layer.objects.active = human
+human.select_set(True)
 
 # Export to FBX
-bpy.ops.export_scene.fbx(filepath="triangle.fbx")
+bpy.ops.export_scene.fbx(filepath="human.fbx")
