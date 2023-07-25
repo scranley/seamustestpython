82dccbd0b434e61420710b627969e0e3628963d0
scranley <80985721+scranley@users.noreply.github.com>
2023-07-25 03:18:46 -0700

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -5,18 +5,25 @@ bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_by_type(type='MESH')
 bpy.ops.object.delete()
 
-# Define the 5 vertices of the pyramid
-verts = [(0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0), (0.5, 0.5, 1)]
+# Define the 3 vertices of the triangle
+verts = [(0, 0, 0), (1, 0, 0), (0.5, 1, 0)]
 
-# Define the 5 faces of the pyramid
-faces = [(0, 1, 4), (1, 2, 4), (2, 3, 4), (3, 0, 4), (0, 1, 2, 3)]
+# Define the single face of the triangle
+faces = [(0, 1, 2)]
 
 # Create the mesh and object
-mesh = bpy.data.meshes.new(name="PyramidMesh")
-obj = bpy.data.objects.new("Pyramid", mesh)
+mesh = bpy.data.meshes.new(name="TriangleMesh")
+obj = bpy.data.objects.new("Triangle", mesh)
 
 # Link the object to the scene
 bpy.context.collection.objects.link(obj)
 
-# Create the pyramid
+# Create the triangle
 mesh.from_pydata(verts, [], faces)
+
+# Select the triangle object
+bpy.context.view_layer.objects.active = obj
+obj.select_set(True)
+
+# Export to FBX
+bpy.ops.export_scene.fbx(filepath="/path/to/triangle.fbx")
